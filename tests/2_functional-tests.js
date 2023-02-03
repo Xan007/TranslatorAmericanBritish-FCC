b9b98a2a133cb5aad9a172ad0c95c970646a64d9
Sander Sierra <sierrasander29@gmail.com>
2023-02-03 16:32:34 -0500

    feat: Adds tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,89 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+    test("Translation with text and locale fields: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post("/api/translate")
+            .send({ text: "We watched the footie match for a while.", locale: "british-to-american" })
+            .end((err, res) => {
+                assert.equal(res.status, 200)
+                assert.isObject(res)
+                assert.property(res.body, "translation")
+
+                done()
+            })
+    })
+
+    test("Translation with text and invalid locale field: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post("/api/translate")
+            .send({ text: "We watched the footie match for a while.", locale: "invalid-locale" })
+            .end((err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "error")
+                assert.isString(res.body.error)
+
+                done()
+            })
+    })
+
+    test("Translation with missing text field: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post("/api/translate")
+            .send({ locale: "british-to-american" })
+            .end((err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "error")
+                assert.isString(res.body.error)
+
+                done()
+            })
+    })
+
+    test("Translation with missing locale field: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post("/api/translate")
+            .send({ text: "We watched the footie match for a while." })
+            .end((err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "error")
+                assert.isString(res.body.error)
+
+                done()
+            })
+    })
+
+    test("Translation with empty text: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post("/api/translate")
+            .send({ text: "", locale: "british-to-american" })
+            .end((err, res) => {
+                assert.equal(res.status, 200)
+                assert.property(res.body, "error")
+                assert.isString(res.body.error)
+
+                done()
+            })
+    })
+
+    test("Translation with text that needs no translation: POST request to /api/translate", (done) => {
+        chai
+            .request(server)
+            .post("/api/translate")
+            .send({ text: "Paracetamol takes up to an hour to work.", locale: "british-to-american" })
+            .end((err, res) => {
+                assert.equal(res.status, 200)
+                assert.isObject(res)
+                assert.property(res.body, "translation")
+                assert.equal(res.body.translation, "Everything looks good to me!")
+
+                done()
+            })
+    })
 
 });
