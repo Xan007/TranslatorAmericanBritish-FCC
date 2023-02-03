0e45e7f22c2e0379766c208cc463c940f8836c00
Sander Sierra <sierrasander29@gmail.com>
2023-02-03 16:32:51 -0500

    fix: translating incorrectly

--- components/translator.js
+++ components/translator.js
@@ -1,9 +1,17 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
 
-const timeRegex = /([\d]{1,2})([.:])([\d]{1,2})/g
+const britishOnly = require('./british-only.js')
+const britishToAmericanSpelling = Object.fromEntries(
+    Object.entries(americanToBritishSpelling).map((k) => k.reverse())
+);
+const britishToAmericanTitles = Object.fromEntries(
+    Object.entries(americanToBritishTitles).map((k) => k.reverse())
+);
+
+const britishTime = /([\d]{1,2}).([\d]{1,2})/g
+const americanTime = /([\d]{1,2}):([\d]{1,2})/g
 
 function addHighlight(word) {
     return `<span class="highlight">${word}</span>`
@@ -13,73 +21,56 @@ function getTimes(text) {
     return [...text.matchAll(timeRegex)]
 }
 
+function capitalize(text) {
+    return text[0].toUpperCase() + text.slice(1);
+}
+
 class Translator {
     constructor(locale) {
         this.locale = locale
     };
 
     translate(text) {
-        const words = text.toLowerCase().split(/[\s"']/)
-
         if (this.locale === "american-to-british")
-            return this.american_to_british(text, words)
+            return this.american_to_british(text)
         else if (this.locale === "british-to-american")
-            return this.british_to_american(text, words)
+            return this.british_to_american(text)
     }
 
-    american_to_british(text, words) {
+    american_to_british(text) {
         let new_text = text
 
-        words.forEach(word => {
-            let new_word = americanOnly[word]
-                || americanToBritishSpelling[word]
-            
-            if (americanToBritishTitles[word])
-                new_word = americanToBritishTitles[word].replace(/^(.)/, (p1) => p1.toUpperCase())
+        for (const [american, british] of Object.entries({
+            ...americanOnly,
+            ...americanToBritishSpelling,
+        })) {
+            new_text = new_text.replaceAll(new RegExp(`[^<-]\\b${american}\\b`, "ig"), " " + addHighlight(british));
+        }
 
-            if (new_word)
-                new_text = new_text.replaceAll(new RegExp(word, "ig"), addHighlight(new_word))
-        })
+        for (const [american, british] of Object.entries(americanToBritishTitles)) {
+            new_text = new_text.replaceAll(new RegExp(american, "ig"), addHighlight(capitalize(british)));
+        }
 
-        getTimes(new_text).forEach((time_match) => {
-            if (time_match[2] == ":")
-                new_text = new_text.replaceAll(
-                    new RegExp(`${time_match[0]}`, "g"),
-                    addHighlight(`${time_match[1]}.${time_match[3]}`)
-                )
-        })
+        new_text = new_text.replaceAll(americanTime, addHighlight("$1.$2"))
 
         return new_text
     }
 
-    british_to_american(text, words) {
+    british_to_american(text) {
         let new_text = text
 
-        function getKeyForValue(value_search, obj) {
-            for (const [key, value] of Object.entries(obj)) {
-                if (value === value_search)
-                    return key
-            }
+        for (const [british, american] of Object.entries({
+            ...britishOnly,
+            ...britishToAmericanSpelling,
+          })) {
+            new_text = new_text.replaceAll(new RegExp(`[^<-]\\b${british}\\b`, "ig"), " " + addHighlight(american));
         }
 
-        words.forEach(word => {
-            let new_word = getKeyForValue(word, britishOnly)
-                || getKeyForValue(word, americanToBritishSpelling)
+        for (const [british, american] of Object.entries(britishToAmericanTitles)) {
+            new_text = new_text.replaceAll(new RegExp(`\\b${british}\\b`, "ig"), addHighlight(capitalize(american)));
+        }
 
-            if (getKeyForValue(word, americanToBritishTitles))
-                new_word = getKeyForValue(word, americanToBritishTitles).replace(/^(.)/, (p1) => p1.toUpperCase())
-
-            if (new_word)
-                new_text = new_text.replaceAll(new RegExp(word, "ig"), addHighlight(new_word))
-        })
-
-        getTimes(new_text).forEach((time_match) => {
-            if (time_match[2] == ".")
-                new_text = new_text.replaceAll(
-                    new RegExp(`${time_match[0]}`, "g"),
-                    addHighlight(`${time_match[1]}:${time_match[3]}`)
-                )
-        })
+        new_text = new_text.replaceAll(britishTime, addHighlight("$1:$2"))
 
         return new_text
     }
