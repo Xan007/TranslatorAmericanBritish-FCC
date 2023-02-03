463d26bdd0be624f18afd6eb01486e534229bd8c
Sander Sierra <sierrasander29@gmail.com>
2023-02-03 13:23:21 -0500

    feat: Endpoints added

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,30 @@
 'use strict';
 
-const Translator = require('../components/translator.js');
+const TranslatorClass = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
+      const { text, locale } = req.body
+
+      if (!(locale !== undefined && text !== undefined))
+        return res.send({ error: 'Required field(s) missing' })
+
+      if (!text)
+        return res.send({ error: 'No text to translate' })
+
+
+      if (!["american-to-british", "british-to-american"].includes(locale))
+        return res.send({ error: 'Invalid value for locale field' })
+
+      const translator = new TranslatorClass(locale)
       
+      let translation = translator.translate(text)
+
+      if (translation === text)
+        translation = "Everything looks good to me!"
+
+      res.send({ text: text, translation: translation })
     });
 };
